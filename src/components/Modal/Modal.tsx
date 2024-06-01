1b757200d814da3dcc359894e767f1373392e6f3
ilyasilkin27 <ilyasilkin27@gmail.com>
2024-06-02 02:36:41 +0300

    fix modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -35,8 +35,8 @@ export const Modal: React.FC<ModalProps> = ({ day, currentDate, onClose }) => {
         <h2>Tasks for {day}</h2>
         <ul>
           {tasks[date]?.map((task: any) => (
-            <li key={task.id} style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
-              <span>{task.text}</span>
+            <li key={task.id} >
+              <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.text}</span>
               <input
                 type="checkbox"
                 checked={task.completed}
@@ -46,13 +46,16 @@ export const Modal: React.FC<ModalProps> = ({ day, currentDate, onClose }) => {
             </li>
           ))}
         </ul>
-        <input
-          type="text"
-          value={newTaskText}
-          onChange={(e) => setNewTaskText(e.target.value)}
-        />
-        {showWarning && <p style={{ color: 'red' }}>Please enter a non-empty task.</p>}
-        <button onClick={handleAddTask}>Add Task</button>
+        <div>
+            <input
+            className='taskInput'
+            type="text"
+            value={newTaskText}
+            onChange={(e) => setNewTaskText(e.target.value)}
+            />
+            {showWarning && <p style={{ color: 'red' }}>Please enter a non-empty task.</p>}
+            <button onClick={handleAddTask}>Add Task</button>
+        </div>
       </div>
     </div>
   );
